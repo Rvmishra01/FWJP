eb4dc6bfc79046f86bf952222da5d00a264a6ca3
Rvmishra01 <ravindermishra360@gmail.com>
2025-01-23 15:32:38 +0530

    Percentage.java

--- Percentage.java
+++ Percentage.java
@@ -1,11 +1,13 @@
-public Percentage {
-    public static void main(String[] args) {
+ Percentage 
+ {
+     static void main(String[] args) 
+     {
         // Define the marks obtained and total marks
         int scoredMark = 430;
         int totalMark = 500;
         
         // Calculate the percentage
          percentage = marksObtained / totalMarks * 100;
-        System.out.println("Percentage: " + percentage + "%");
-    }
+        System.out.println("Percentage: " + percentage );
+      }
 }
