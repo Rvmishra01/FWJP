2efd0adb440f20f6026689b08605d51619760882
Rvmishra01 <ravindermishra360@gmail.com>
2025-01-23 15:20:54 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,11 @@
+public Percentage {
+    public static void main(String[] args) {
+        // Define the marks obtained and total marks
+        int scoredMark = 430;
+        int totalMark = 500;
+        
+        // Calculate the percentage
+         percentage = marksObtained / totalMarks * 100;
+        System.out.println("Percentage: " + percentage + "%");
+    }
+}
